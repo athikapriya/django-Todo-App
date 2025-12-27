58a1adf5214cd71b042060b4ab91e376888482f4
athikapriya <athikapriya1997@gmail.com>
2025-12-27 16:05:40 +0600

    CRUD urls added

--- base/urls.py
+++ base/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path("overdue/", views.overdue, name='overdue'),
 
     path('toggle_task/<int:task_id>/', views.toggle_task, name="toggle_task"),
+
+    path('create_task/', views.create_task, name="create_task"),
+    path("update_task/<str:pk>/", views.update_task, name="update_task"),
+    path("delete_task/<str:pk>/", views.delete_task, name="delete_task"),
 ]
