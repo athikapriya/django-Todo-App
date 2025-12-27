0bb211d5a99e700b7913c73ad8a1a20a28965a26
athikapriya <athikapriya1997@gmail.com>
2025-12-27 16:04:15 +0600

    models.py modified

--- base/models.py
+++ base/models.py
@@ -1,18 +1,29 @@
 from django.db import models
 from django.utils import timezone
+from datetime import timedelta
 
 # Create your models here.
+
+def default_deadline():
+    return timezone.now() + timedelta(hours=24)
+
+
 class Task(models.Model):
+
     PRIORITY_CHOICES = (
         ("Low", "Low"),
         ("Medium", "Medium"),
         ("High", "High")
     )
+
     name = models.CharField(max_length=100, blank=False)
     description = models.TextField(max_length=200,  blank=True)
-    deadline = models.DateTimeField(null=True, blank=True)
+
+    deadline = models.DateTimeField(default=default_deadline, null=True, blank=True)
+
     is_completed = models.BooleanField(default=False)
-    priority = models.CharField(max_length=50, choices=PRIORITY_CHOICES, default="medium")
+    priority = models.CharField(max_length=50, choices=PRIORITY_CHOICES, default="Medium")
+
     created_at = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
