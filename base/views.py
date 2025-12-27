b663d4f47a23b0e96aa63fb447ea443f9e7ae8aa
athikapriya <athikapriya1997@gmail.com>
2025-12-27 16:05:12 +0600

    CURD functionality views added

--- base/views.py
+++ base/views.py
@@ -1,28 +1,44 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.utils import timezone
 from .models import *
-
+from .forms import TaskForm
+from .filters import TaskFilter
 
 # Create your views here.
 def homepage(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.filter(is_completed=False)
+    
+    completed_tasks = Task.objects.filter(is_completed=True)
+
+    myFilter = TaskFilter(request.GET, queryset = tasks)
+    tasks = myFilter.qs
+
     context = {
         "active_page" : "homepage",
-        "tasks" : tasks
+        "tasks" : tasks,
+        "completed_tasks" : completed_tasks,
+        "myFilter" : myFilter
     }
     return render(request, 'base/homepage.html', context)
 
 
 def pending(request):
+    time = timezone.now()
+    pending_tasks = Task.objects.filter(deadline__gte=time, is_completed=False)
     context = {
-        "active_page" : "pending"
+        "active_page" : "pending",
+        "pending_tasks" : pending_tasks
     }
     return render(request, 'base/pending.html', context)
 
 
 
 def overdue(request):
+    time = timezone.now()
+    overdue_tasks = Task.objects.filter(deadline__lt=time, is_completed=False)
     context = {
-        "active_page" : "overdue"
+        "active_page" : "overdue",
+        "overdue_tasks" : overdue_tasks
     }
     return render(request, 'base/overdue.html', context)
 
@@ -37,3 +53,47 @@ def toggle_task(request, task_id):
 
     context = {}
     return redirect(request.META.get('HTTP_REFERER', '/'))
+
+
+
+def create_task(request):
+    form = TaskForm()
+
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("homepage")
+
+    context = {
+        'form' : form
+    }
+    return render(request, 'base/task_form.html', context)
+
+
+
+
+def update_task(request, pk):
+    task = Task.objects.get(id=pk)
+    form = TaskForm(instance=task)
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect("homepage")
+    context = {
+        "form" : form
+    }
+    return render(request, 'base/task_form.html', context)
+
+
+
+def delete_task(request, pk):
+    task = Task.objects.get(id=pk)
+    if request.method == 'POST':
+        task.delete()
+        return redirect("/")
+    context = {
+        'task' : task
+    }
+    return render(request, 'base/delete_task.html', context)
